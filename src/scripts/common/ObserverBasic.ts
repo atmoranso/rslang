7712464e68045a14dee80b0bfe999f50a5c22f71
Atmoranso <atmoranso@gmail.com>
2022-08-18 02:22:56 +0300

    refactor: change naming in observer class

--- src/scripts/common/ObserverBasic.ts
+++ src/scripts/common/ObserverBasic.ts
@@ -5,15 +5,15 @@ export default class ObserverBasic<ListenerType> {
     this.listeners = [];
   }
 
-  add(listener: (params: ListenerType) => void) {
+  subscribe(listener: (params: ListenerType) => void) {
     this.listeners.push(listener);
   }
 
-  remove(listener: (params: ListenerType) => void) {
+  unsubscribe(listener: (params: ListenerType) => void) {
     this.listeners = this.listeners.filter((item) => item !== listener);
   }
 
-  emit(params: ListenerType) {
+  notify(params: ListenerType) {
     this.listeners.forEach((listener) => listener(params));
   }
 }
