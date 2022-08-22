5621e33c5630155a8f336203f9fc4aa7931cf4da
redcliphaloe <redcliphaloe@gmail.com>
2022-08-23 02:04:52 +0300

    feat: add Textbook.ts

---
+++ src/scripts/modules/textBook/TextBook.ts
@@ -0,0 +1,13 @@
+import ElementTemplate from '../../common/ElementTemplate';
+import { Module } from '../../common/types';
+import TextbookView from './view/TextbookView';
+
+export default class Textbook implements Module {
+  view: ElementTemplate;
+
+  constructor() {
+    this.view = new TextbookView(document.body);
+  }
+
+  start() {}
+}
