5cb2a58cdd38d34ef415c895f6acd8c5c22cfa72
Kirsawka <83959481+Kirsawka@users.noreply.github.com>
2022-08-22 23:04:37 +0300

    feat: implement basic views, main classes

---
+++ src/scripts/modules/audioCall/AudioCall.ts
@@ -0,0 +1,13 @@
+import ElementTemplate from '../../common/ElementTemplate';
+import { Module } from '../../common/types';
+import AudioCallView from "./view/AudioCallView";
+
+export default class AudioCall implements Module {
+  view: ElementTemplate;
+
+  constructor() {
+    this.view = new AudioCallView(null);
+  }
+
+  start() {}
+}
---
+++ src/scripts/modules/statistic/view/StatisticView.ts
@@ -0,0 +1,7 @@
+import ElementTemplate from '../../../common/ElementTemplate';
+
+export default class StatisticView extends ElementTemplate {
+  constructor(parentNode: HTMLElement | null) {
+    super(parentNode, 'div', '', 'Статистика');
+  }
+}
