72e9b6330fe2d165f2f57d4a72ccdf3297df002e
Atmoranso <atmoranso@gmail.com>
2022-09-01 00:44:58 +0300

    feat: add class ModuleClass as interface

--- src/scripts/app/router/Router.ts
+++ src/scripts/app/router/Router.ts
@@ -3,11 +3,13 @@ import ErrorPage from '../../modules/errorPage/ErrorPage';
 import Home from '../../modules/home/Home';
 import Textbook from '../../modules/textBook/TextBook';
 import AppView from '../view/AppView';
+import state from '../../common/state';
+import { AppState } from '../../common/stateTypes';
 
 export default class Router {
   view: AppView;
 
-  private routes: Record<string, new () => Module> = {
+  private routes: Record<string, new (state: AppState) => Module> = {
     '404': ErrorPage,
     '': Home,
     textbook: Textbook,
@@ -20,7 +22,7 @@ export default class Router {
   router = () => {
     const path = window.location.hash.slice(1);
     const route = this.routes[path] ? this.routes[path] : this.routes['404'];
-    const module = new route();
+    const module = new route(state);
     module.start();
     this.view.update(module.view);
   };
