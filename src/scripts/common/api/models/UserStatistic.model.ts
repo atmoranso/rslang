b0b9c111910a7009584d1849490986be9742ef6a
Atmoranso <atmoranso@gmail.com>
2022-09-01 23:17:50 +0300

    feat: change UserStatistic interface

--- src/scripts/common/api/models/UserStatistic.model.ts
+++ src/scripts/common/api/models/UserStatistic.model.ts
@@ -1,6 +1,42 @@
+enum Month {
+  January,
+  February,
+  March,
+  April,
+  May,
+  June,
+  July,
+  August,
+  September,
+  October,
+  November,
+  December,
+}
+
+interface DailyGameStat {
+  newWords: number;
+  correctAnswers: number;
+  incorrectAnswers: number;
+  mostLongCorrectChain: number;
+}
+interface DailyStat {
+  newWords: number;
+  learnedWords: number;
+  correctAnswers: number;
+  incorrectAnswers: number;
+  sprint: DailyGameStat;
+}
+
+interface totalDayStat {
+  newWords: number;
+  learnedWords: number;
+}
 interface UserStatistic {
   learnedWords: number;
-  optional: Record<string, unknown>;
+  optional: {
+    dailyStat: DailyStat;
+    totalStat: Record<Month, Record<number, totalDayStat>>;
+  };
 }
 
 export default UserStatistic;
