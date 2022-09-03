48f658cb28b7a52df0b5e190fb93790d83ae9e96
Kirsawka <83959481+Kirsawka@users.noreply.github.com>
2022-09-03 19:05:00 +0300

    fix/finish header-footer-responsive

--- src/scripts/app/model/AppModel.ts
+++ src/scripts/app/model/AppModel.ts
@@ -1,1 +1,8 @@
-export default class AppModel {}
+export default class AppModel {
+  toggleOpenLockClasses(burger: HTMLElement, menu: HTMLElement, overlay: HTMLElement) {
+    burger.classList.toggle('open');
+    menu.classList.toggle('open');
+    overlay.classList.toggle('open');
+    document.body.classList.toggle('lock');
+  }
+}
